495538775e4dbaa763ccc6c64feda730520fb096
Robert Dawson <manatees@gmail.com>
2021-09-09 23:41:34 -0700

    initial commit

---
+++ cartDB/my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7c4cc86a8d266b77426ac757fe238b91'),
+    },
+  },
+});
---
+++ cartDB/my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '36e47c74-bec7-4c46-8c23-215af29c1920'
+};
